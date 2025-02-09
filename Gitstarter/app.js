f246fc42a8656e2f732dbff7c02c2a89a9b3e82b
Abhay <abhay.official005@gmail.com>
2025-02-09 13:28:34 +0530

    first code changes

--- Gitstarter/app.js
+++ Gitstarter/app.js
@@ -1,1 +1,16 @@
 console.log("welcome")
+
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+
+header.style.borderBottom = ('3px solid orange');
+
+const mainHeading = document.getElementById('main-heading');
+mainHeading.textContent = ('Fruit World');
+mainHeading.style.color = "orange";
+
+const basketHeading = document.getElementById('basket-heading');
+basketHeading.style.color = ('green');
+
+const thanking = document.getElementById('thanks');
+thanking.innerHTML = ('<p>Please visit us again</p>')
