ed994f50f0196a01c8c878d0fcb0589017405c94
Abhay <abhay.official005@gmail.com>
2025-02-09 21:36:42 +0530

    getElementsByTagName

--- Gitstarter/app.js
+++ Gitstarter/app.js
@@ -1,4 +1,5 @@
 console.log("welcome")
+#get elements by id method
 
 const header = document.getElementById('header');
 header.style.backgroundColor = 'green';
@@ -14,3 +15,11 @@ basketHeading.style.color = ('green');
 
 const thanking = document.getElementById('thanks');
 thanking.innerHTML = ('<p>Please visit us again</p>')
+
+#get elements by tagname method
+
+const listitem = document.getElementsByTagName('li');
+listitem[4].style.color = ('blue');
+for (let i = 0; i < listitem.length; i++){
+    listitem[i].style.fontStyle = 'italic';
+} 
